c46f008e51cda80b7819539b1eb770187174e185
Wesley-Giese-ICD2O1-02 <Wesley.giese@student.tdsb.on.ca>
2024-11-08 18:52:34 +0000

    py

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,54 @@
+'''
+    Lesson:  Bolleans
+    Author: Wesley Giese
+    Date Created: September 25, 2024
+    Date Last Modified: november 8, 2024
+'''
 
 
 def q1():
   #Write code here
+  bool1 = True
+  print(bool1)
 
 def q2():
   #Write code here
+  word2 = True
+  word2 > 5
+  print(word2)
+  word2 = False
+  word2 < 5
+  print(word2)
+
 
 def q3():
   #Write code here
+  word3 = input("Input the letter a:")
+  word3 = bool(True)
+  bool(word3) == a
+  print(word3)
+
 
 def q4():
   #Write code here
+  word4 = input("Input a word earlier in the dictionary than google: ")
+  word4 = bool(True)
+  print(word4)
+
 
 def q5():
   #Write code here
+  word5 = input("Input an Integer: ")
+  word6 = input("Input another Integer: ")
+  bool5 = (word5 * word6) > 40
+  print(f"Your numbers multiplied together are greater than 40: {bool5}")
+
 
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
